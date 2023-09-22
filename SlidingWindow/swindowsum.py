8c4469a95ba0f055157cc1de655ad2766eb678d7
peatey <128731828+peat-code@users.noreply.github.com>
2023-09-22 15:26:33 +0530

    Update swindowsum.py

--- SlidingWindow/swindowsum.py
+++ SlidingWindow/swindowsum.py
@@ -1,13 +1,13 @@
-        i = 0
-        j = 0
-        csum = 0
-        length  = -1
-        while j<len(nums):    
-            csum += nums[j]
-            j += 1
-            while i<=j and csum > tofind:
-                csum -= nums[i]
-                i += 1
-            if csum == tofind:
-                length = max(length,j-i)
-            
+i = 0
+j = 0
+csum = 0
+length  = -1
+while j<len(nums):    
+    csum += nums[j]
+    j += 1
+    while i<=j and csum > tofind:
+        csum -= nums[i]
+        i += 1
+    if csum == tofind:
+        length = max(length,j-i)
+    
